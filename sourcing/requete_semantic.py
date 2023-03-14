09ae450459c49ad194b20b1c6592f106b5ecca34
Can <can.baskurt@gmail.com>
2023-03-14 09:11:51 +0000

    Finalisation de la fonction d'obtention du concept de type descriptor et création et stockage d'un clean json  .  next: faire les autres types de concept

--- sourcing/requete_semantic.py
+++ sourcing/requete_semantic.py
@@ -31,70 +31,111 @@ key_api = os.getenv("KEY_API_NYT")
 params_req = {"api-key" : key_api, "fields" : "article_list"}
 
 
-
 def getConceptDes(concept):
     conceptName=concept
-    #url_api_concept = "http://api.nytimes.com/svc/semantic/v2/concept/name/nytd_des/{conceptName}.json?"
-    url_api_concept = "http://api.nytimes.com/svc/semantic/v2/concept/name/nytd_des/Coronavirus (2019-nCoV).json?"
+    print(concept)
+    url_base = "http://api.nytimes.com/svc/semantic/v2/concept/name/nytd_des/"
+    url_end = ".json?"
+    url_api_concept = url_base + concept + url_end
+    print(url_api_concept)
+    #url_api_concept = "http://api.nytimes.com/svc/semantic/v2/concept/name/nytd_des/Coronavirus (2019-nCoV).json?"
     requestConcept = requests.get(url_api_concept, params=params_req)
     return requestConcept
 
 
 
-#Stockage dans un fichier "ConceptTest.json"
-def writeRawConceptToJSON(requestConcept):
+def writeRawConceptToJSON(requestConcept, rawJsonName):
+    """
+    Stockage dans un fichier "ConceptRaw.json"
+    """
     conceptJSON = requestConcept.json()
-    with open("conceptRaw.json", "w") as conceptJSONDoc:
+    with open("../data/raw_data/conceptRaw_"+rawJsonName+".json", "w") as conceptJSONDoc:
         json.dump(conceptJSON, conceptJSONDoc)
         return conceptJSON
 
 
-#Obtention des éléments du concept
-#Fonction qui prend en argument un concept (string) de type des (descriptor) et qui renvoi certains éléments du concept suivant l'API semantic du NY Times. 
-#Les éléments renvoyés sont les suivants:
-# concept_id
-#-concept_name
-#-concept_created
-#-concept_status
-#-concept_type
-#-concept_uri
-#-article_list:
-#   -body
-#   -concepts:
-#       -nydt_des[]
-#       -nydt_geo[]
-#       -nydt_org[]
-#       -nydt_per[]
-#   -date
-#   -title
-#   -type_of_material
-#   -url
+
 def getElementsFromConcept(rawJSONConcept):
+    """
+    Obtention des éléments du concept
+    Fonction qui prend en argument un concept (string) de type des (descriptor) et qui renvoi certains éléments du concept suivant l'API semantic du NY Times. 
+    Les éléments renvoyés sont les suivants:
+     -concept_id
+     -concept_name
+     -concept_created
+     -concept_status
+     -concept_type
+     -concept_uri
+     -article_list:
+        -body
+        -concepts:
+            -nydt_des[]
+            -nydt_geo[]
+            -nydt_org[]
+            -nydt_per[]
+        -date
+        -title
+        -type_of_material
+        -url        
+    """
     results = rawJSONConcept['results']
-    
-    conceptPropre = {}
-
-    concept_id = results[0]['concept_id']
-    concept_name = results[0]['concept_name']
-    concept_created = results[0]['concept_created']
-    concept_status = results[0]['concept_status']
-    concept_type = results[0]['concept_type']
-    concept_uri = results[0]['concept_uri']
-
-    print(concept_id)
     #pprint(rawJSONConcept)
 
-    #conceptPropre["concept_id" : "results[0]['concept_id']"]
-    #conceptPropre["concept_name" : concept_name]
-    pprint(conceptPropre)
+    article_list_raw = results[0]['article_list']['results']
+    #print(len(article_list_raw))
+
+    article_list_clean = []
+
+    #boucler sur les articles de la article_list avec limite sur le length pour pas aller out of bounds
+    for index, article in zip(range(len(article_list_raw)), article_list_raw):
+        #pprint(article['title'])
+        article = {}
+        article['body'] = article_list_raw[0]['body']
+        article['date'] = article_list_raw[0]['date']
+        article['title'] = article_list_raw[0]['title']
+        article['type_of_material'] = article_list_raw[0]['type_of_material']
+        article['url'] = article_list_raw[0]['url']
+        article['linkedConcepts'] = article_list_raw[0]['concepts']
+        article_list_clean.append(article)
     
+    #print(len(article_list_clean))
+    #pprint(article_list_clean)
+
+    conceptPropre = {}
+
+    conceptPropre["concept_id"] = results[0]['concept_id']
+    conceptPropre["concept_name"] = results[0]['concept_name']
+    conceptPropre["concept_created"] = results[0]['concept_created']
+    conceptPropre["concept_status"] = results[0]['concept_status']
+    conceptPropre["concept_type"] = results[0]['concept_type']
+    conceptPropre["concept_uri"] = results[0]['concept_uri']
+    conceptPropre["article_list"] = article_list_clean
+
     return conceptPropre
     
-    
 
-conceptString = getConceptDes("Coronavirus (2019-nCoV)")
-print(conceptString)
-rawJSON = writeRawConceptToJSON(conceptString)
-#print(rawJSON)
-dictConceptPropre = getElementsFromConcept(rawJSON)
-pprint(dictConceptPropre)
+def writeCleanConceptToJSON(cleanConcept, jsonName):
+    with open(jsonName+".json", "w") as cleanConceptJSONDoc:
+        json.dump(cleanConcept, cleanConceptJSONDoc)
+
+
+
+
+def descriptorConceptToCleanJson(concept):
+    """
+    Fonction complète qui récupère le concept de type descriptor et crée et stock le JSON clean avec ce concept
+    """
+    conceptString = getConceptDes(concept)
+    print(conceptString)
+
+    rawJSON = writeRawConceptToJSON(conceptString, concept)
+    #print(rawJSON)
+    dictConceptPropre = getElementsFromConcept(rawJSON)
+    #pprint(dictConceptPropre)
+    writeCleanConceptToJSON(dictConceptPropre, concept)
+
+
+concept = "Coronavirus (2019-nCoV)"
+descriptorConceptToCleanJson(concept)
+concept2 = "Baseball"
+descriptorConceptToCleanJson(concept2)
