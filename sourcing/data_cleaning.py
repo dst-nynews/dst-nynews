1faf21aae812d7129e64c2c5630fb29295b33db1
cafetierePhilo <guiraud.clement31@gmail.com>
2023-03-27 15:44:31 +0200

    POO de data_cleaning.py

--- sourcing/data_cleaning.py
+++ sourcing/data_cleaning.py
@@ -8,62 +8,68 @@ import json
 import os
 
 
-# récupération fichiers json
-def import_json(file_path):
-    with open(file_path, "r") as file:
-        file_json = json.load(file)
-    return file_json
-
-#récupération du nom
-def file_name(file_path):
-    name = os.path.basename(file_path)
-    return name
+class ToCleanJsonArticleSearch:
+    """
+    """
+    def __init__(self) -> None:
+        pass
 
 
+    def import_json(self,file_path):
+        with open(file_path, "r") as file:
+            file_json = json.load(file)
+        return file_json
 
 
 
-# clean des données
-def json_clean(file_json):
-    json_first_step = file_json["response"]["docs"]
-    if json_first_step == []:
-        return print("Fichier vide")
-    else :
-        list_articles_cleaned = []
-        for i in json_first_step:
-            article={}
-            article["abstract"] = i.get("abstract")
-            article["web_url"] = i.get("web_url")
-            article["snippet"] = i.get("snippet")
-            article["lead_paragraph"] = i.get("lead_paragraph")
-            article["print_section"] = i.get("print_section")
-            article["print_page"] = i.get("print_page")
-            article["headline"] = i.get("headline")
-            article["keywords"] = i.get("keywords")
-            article["pub_date"] = i.get("pub_date")
-            article["document_type"] = i.get("document_type")
-            article["news_desk"] = i.get("news_desk")
-            article["section_name"] =i.get("section_name")
-            article["subsection_name"] = i.get("subsection_name")
-            article["id"] = i.get("_id")
-            article["word_count"] = i.get("word_count")
-            article["uri"] = i.get("uri")
+    def file_name(self,file_path):
+        name = os.path.basename(file_path)
+        return name
 
-            list_articles_cleaned.append(article)
+    def json_clean(self,file_json):
+        json_first_step = file_json["response"]["docs"]
+        if json_first_step == []:
+            return print("Fichier vide")
+        else :
+            list_articles_cleaned = []
+            for i in json_first_step:
+                article={}
+                article["abstract"] = i.get("abstract")
+                article["web_url"] = i.get("web_url")
+                article["snippet"] = i.get("snippet")
+                article["lead_paragraph"] = i.get("lead_paragraph")
+                article["print_section"] = i.get("print_section")
+                article["print_page"] = i.get("print_page")
+                article["headline"] = i.get("headline")
+                article["keywords"] = i.get("keywords")
+                article["pub_date"] = i.get("pub_date")
+                article["document_type"] = i.get("document_type")
+                article["news_desk"] = i.get("news_desk")
+                article["section_name"] =i.get("section_name")
+                article["subsection_name"] = i.get("subsection_name")
+                article["id"] = i.get("_id")
+                article["word_count"] = i.get("word_count")
+                article["uri"] = i.get("uri")
 
-        return list_articles_cleaned
+                list_articles_cleaned.append(article)
+
+            return list_articles_cleaned
     
 
-#stockage dans json
-def to_clean_json(clean_json,file_name):
-    with open("data/cleaned_data/cleaned_"+file_name+".json", "w") as f:
-        json.dump(clean_json,f)
+    def to_clean_json(self,clean_json,file_name):
+        with open("data/cleaned_data/cleaned_"+file_name+".json", "w") as f:
+            json.dump(clean_json,f)
 
 
 
-def from_raw_to_clean(file):
-    to_treat = import_json(file)
-    name = file_name(file)
-    to_stock = json_clean(to_treat)
-    to_clean_json(to_stock, name)
+    def from_raw_to_clean(self,file):
+        to_treat = self.import_json(file)
+        name = self.file_name(file)
+        to_stock = self.json_clean(to_treat)
+        self.to_clean_json(to_stock, name)
 
+
+tcas = ToCleanJsonArticleSearch()
+test = "data/raw_data/Covid2020-01-31_Page_0.json"
+
+tcas.from_raw_to_clean(test)
