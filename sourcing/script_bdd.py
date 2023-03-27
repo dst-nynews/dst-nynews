7d1d20099e72ebdb6951e70a47b188d7f0bd036f
cafetierePhilo <guiraud.clement31@gmail.com>
2023-03-27 14:21:48 +0200

    Mise en forme POO pour utilisation par tout le monde

--- sourcing/script_bdd.py
+++ sourcing/script_bdd.py
@@ -4,33 +4,44 @@ Script récupérant les fichiers Json de article search pour les mettre dans une
 from pymongo import MongoClient
 import json
 
-client = MongoClient(
-    host="127.0.0.1",
-    port = 27017
-)
 
-db = client['NYTimes']
-article_search = db['article_search']
-
-# récupération fichiers json
 def import_json(file_name):
     with open(file_name, "r") as file:
         file_json = json.load(file)
     return file_json
 
-# insertion dans la bdd
-def insert_mongoDB(json_cleaned):
-    if isinstance(json_cleaned, list):
-        article_search.insert_many(json_cleaned) 
-    else:
-        article_search.insert_one(json_cleaned)
+
+
+class ImportMongDB:
+
+    def __init__(self,coll,db="NYTimes",host = "127.0.0.1",port =27017 ) -> None:
+        self.host = host
+        self.port = port 
+        self.db = db 
+        self.coll = coll
+
+   
+    # insertion dans la bdd
     
+    def insert_mongoDB(self,json_cleaned):
+        client = MongoClient(
+            host= self.host,
+            port = self.port
+        )
+        db = client[self.db]
+        collection = db[self.coll]
+    
+        if isinstance(json_cleaned, list):
+            collection.insert_many(json_cleaned) 
+        else:
+            collection.insert_one(json_cleaned)
 
-file = import_json("../data/raw_data/covid20192023_Page_0.json")
-file_cleaned = json_clean(file)
-insert_mongoDB(file_cleaned)
 
-print(client.list_database_names())
+
+#Test
+json_cleaned = import_json("data/raw_data/Covid2020-01-31_Page_0.json")
+test = ImportMongDB("ArticleSearch")
+test.insert_mongoDB(json_cleaned)
 
 
 
