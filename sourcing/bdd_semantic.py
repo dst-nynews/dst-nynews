3dabfcb808ec524f69ae7e82b9ec8fbed557564a
Can <can.baskurt@gmail.com>
2023-03-28 16:38:28 +0000

    Creation des classes APISemantic et BddSemantic

--- sourcing/bdd_semantic.py
+++ sourcing/bdd_semantic.py
@@ -8,35 +8,54 @@ Fichier qui va faire le lien entre la BD Mongo et les élements de l'API semanti
 from pymongo import MongoClient
 import json
 from datetime import datetime
+import glob
+from typing import Dict, Optional
+import os
+from dotenv import load_dotenv
 
-def import_json(file_name):
-        with open(file_name, "r") as file:
-            file_json = json.load(file)
-        return file_json
+load_dotenv()
+
+class BddSemantic:
+    def __init__(self, repo_path_clean: Optional[str] = None) -> None:
+        """Instanciate a connection to fetch data from an API of the NY Times.
+        Args:
+            repo_path (Optional[str], optional): Path to clean storage directory.
+        """
+        self.repo_path_clean = repo_path_clean
+        self.MongoHost = os.getenv("MONGO_CLIENT_HOST")
+        self.Client = MongoClient(host=self.MongoHost, port=27017)
+
+    def import_json(self, file_name):
+            with open(self.repo_path_clean+file_name, "r") as file:
+                file_json = json.load(file)
+            return file_json
 
 
-def insert_mongoDB(json_cleaned, collection):
-        to_stock = import_json(json_cleaned)
-        if to_stock != None:    
-            if isinstance(to_stock, list):
-                for document in to_stock:
-                     document["created_at"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-                     collection.replace_one({"_id": document["_id"]}, document, upsert = True)
-            else:
-                #collection.insert_one(to_stock)
-                to_stock["created_at"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-                collection.replace_one({"_id": to_stock["_id"]}, to_stock, upsert = True)
+    def insert_mongoDB(self, json_cleaned, collection):
+            to_stock = self.import_json(json_cleaned)
+            if to_stock != None:    
+                if isinstance(to_stock, list):
+                    for document in to_stock:
+                        document["created_at"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                        collection.replace_one({"_id": document["_id"]}, document, upsert = True)
+                else:
+                    #collection.insert_one(to_stock)
+                    to_stock["created_at"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                    collection.replace_one({"_id": to_stock["_id"]}, to_stock, upsert = True)
 
 
 # test
-client = MongoClient(host="mongodb+srv://canbaskurt:wxsNxL2fMLq6usJU@cluster0.zjdexu5.mongodb.net", port=27017)
+bddSemantic = BddSemantic("../data/clean_data/")
+db = bddSemantic.Client['NYTimes']
+print(db.list_collection_names())
 
-db = client['NYTimes']
 concepts = db['Concepts']
 searchSemantic = db["SearchSemantic"]
 
-print(client.list_database_names())
-print(db.list_collection_names())
+bddSemantic.insert_mongoDB("Baseball.json", concepts)
+bddSemantic.insert_mongoDB("Coronavirus.json", searchSemantic)
 
-insert_mongoDB("Baseball.json", concepts)
-insert_mongoDB("Coronavirus.json", searchSemantic)
+#articles = db['Articles']
+#liste_cleaned = glob.glob("data/cleaned_data/cleaned*")
+#for i in liste_cleaned:
+#    insert_mongoDB(i,articles)
--- sourcing/requete_semantic.py
+++ sourcing/requete_semantic.py
@@ -34,227 +34,242 @@ import json
 import os
 from pprint import pprint
 from dotenv import load_dotenv
+from typing import Dict, Optional
 load_dotenv()
 
-key_api = os.getenv("KEY_API_NYT")
-params_req = {"api-key" : key_api, "fields" : "article_list"}
+class ApiSemantic:
+    def __init__(self, repo_path_raw: Optional[str] = None, repo_path_clean: Optional[str] = None) -> None:
+        """Instanciate a connection to fetch data from an API of the NY Times.
+        Args:
+            repo_path (Optional[str], optional): Path to storage directory.
+        """
+
+        self.key_api = os.getenv("KEY_API_NYT")
+        self.params_req = {"api-key" : self.key_api, "fields" : "article_list"}
+        self.repo_path_raw = repo_path_raw
+        self.repo_path_clean = repo_path_clean
+
+
 
 # A: requête type/concept
-# étape 1
-def get_concept(concept, type):
-    """
-    On utilise l'api semantic avec la méthode GET /name/{concept-type}/{specific-concept}.json
-    """
-    #print(concept)
-    #print(type)
+    # étape 1
+    def get_concept(self, concept, type):
+        """
+        On utilise l'api semantic avec la méthode GET /name/{concept-type}/{specific-concept}.json
+        """
+        #print(concept)
+        #print(type)
+        
+        conceptName = concept
+        conceptType = type+"/"
+
+        url_base = "http://api.nytimes.com/svc/semantic/v2/concept/name/"
+        url_end = ".json?"
+        url_api_concept = url_base + conceptType + conceptName + url_end
+        print(url_api_concept)
+        #url_api_concept = "http://api.nytimes.com/svc/semantic/v2/concept/name/nytd_des/Coronavirus (2019-nCoV).json?"
+        requestConcept = requests.get(url_api_concept, params= self.params_req)
+        return requestConcept
+
+    # étape 2
+    def write_raw_concept_to_JSON(self, requestConcept, rawJsonName):
+        """
+        Stockage dans un fichier "ConceptRaw.json"
+        """
+        conceptJSON = requestConcept.json()
+        with open(self.repo_path_raw+"conceptRaw_"+rawJsonName+".json", "w") as conceptJSONDoc:
+            json.dump(conceptJSON, conceptJSONDoc)
+            return conceptJSON
+
+    # étape 3    
+    def get_elements_from_concept(self, rawJSONConcept):
+        """
+        Obtention des éléments du concept
+        Fonction qui prend en argument un concept (string) et qui renvoi certains éléments du concept suivant l'API semantic du NY Times. 
+        Les éléments renvoyés sont les suivants:
+        -concept_id
+        -concept_name
+        -concept_created
+        -concept_status
+        -concept_type
+        -concept_uri
+        -article_list:
+            -body
+            -concepts:
+                -nytd_des[]
+                -nytd_geo[]
+                -nytd_org[]
+                -nytd_per[]
+            -date
+            -title
+            -type_of_material
+            -url        
+        """
+        results = rawJSONConcept['results']
+        #pprint(rawJSONConcept)
+
+        article_list_raw = results[0]['article_list']['results']
+        #print(len(article_list_raw))
+
+        article_list_clean = []
+
+        #boucler sur les articles de la article_list avec limite sur le length pour pas aller out of bounds
+        for index, article in zip(range(len(article_list_raw)), article_list_raw):
+            #pprint(article['title'])
+            article = {}
+            article['body'] = article_list_raw[0]['body']
+            article['date'] = article_list_raw[0]['date']
+            article['title'] = article_list_raw[0]['title']
+            article['type_of_material'] = article_list_raw[0]['type_of_material']
+            article['url'] = article_list_raw[0]['url']
+            article['linkedConcepts'] = article_list_raw[0]['concepts']
+            article_list_clean.append(article)
+        
+        #print(len(article_list_clean))
+        #pprint(article_list_clean)
+
+        conceptPropre = {}
+
+        conceptPropre["_id"] = results[0]['concept_id']
+        conceptPropre["concept_name"] = results[0]['concept_name']
+        conceptPropre["concept_created"] = results[0]['concept_created']
+        conceptPropre["concept_status"] = results[0]['concept_status']
+        conceptPropre["concept_type"] = results[0]['concept_type']
+        conceptPropre["concept_uri"] = results[0]['concept_uri']
+        conceptPropre["article_list"] = article_list_clean
+
+        return conceptPropre
     
-    conceptName = concept
-    conceptType = type+"/"
-
-    url_base = "http://api.nytimes.com/svc/semantic/v2/concept/name/"
-    url_end = ".json?"
-    url_api_concept = url_base + conceptType + conceptName + url_end
-    print(url_api_concept)
-    #url_api_concept = "http://api.nytimes.com/svc/semantic/v2/concept/name/nytd_des/Coronavirus (2019-nCoV).json?"
-    requestConcept = requests.get(url_api_concept, params=params_req)
-    return requestConcept
-
-# étape 2
-def write_raw_concept_to_JSON(requestConcept, rawJsonName):
-    """
-    Stockage dans un fichier "ConceptRaw.json"
-    """
-    conceptJSON = requestConcept.json()
-    with open("../data/raw_data/conceptRaw_"+rawJsonName+".json", "w") as conceptJSONDoc:
-        json.dump(conceptJSON, conceptJSONDoc)
-        return conceptJSON
-
-# étape 3    
-def get_elements_from_concept(rawJSONConcept):
-    """
-    Obtention des éléments du concept
-    Fonction qui prend en argument un concept (string) et qui renvoi certains éléments du concept suivant l'API semantic du NY Times. 
-    Les éléments renvoyés sont les suivants:
-     -concept_id
-     -concept_name
-     -concept_created
-     -concept_status
-     -concept_type
-     -concept_uri
-     -article_list:
-        -body
-        -concepts:
-            -nytd_des[]
-            -nytd_geo[]
-            -nytd_org[]
-            -nytd_per[]
-        -date
-        -title
-        -type_of_material
-        -url        
-    """
-    results = rawJSONConcept['results']
-    #pprint(rawJSONConcept)
-
-    article_list_raw = results[0]['article_list']['results']
-    #print(len(article_list_raw))
-
-    article_list_clean = []
-
-    #boucler sur les articles de la article_list avec limite sur le length pour pas aller out of bounds
-    for index, article in zip(range(len(article_list_raw)), article_list_raw):
-        #pprint(article['title'])
-        article = {}
-        article['body'] = article_list_raw[0]['body']
-        article['date'] = article_list_raw[0]['date']
-        article['title'] = article_list_raw[0]['title']
-        article['type_of_material'] = article_list_raw[0]['type_of_material']
-        article['url'] = article_list_raw[0]['url']
-        article['linkedConcepts'] = article_list_raw[0]['concepts']
-        article_list_clean.append(article)
-    
-    #print(len(article_list_clean))
-    #pprint(article_list_clean)
-
-    conceptPropre = {}
-
-    conceptPropre["_id"] = results[0]['concept_id']
-    conceptPropre["concept_name"] = results[0]['concept_name']
-    conceptPropre["concept_created"] = results[0]['concept_created']
-    conceptPropre["concept_status"] = results[0]['concept_status']
-    conceptPropre["concept_type"] = results[0]['concept_type']
-    conceptPropre["concept_uri"] = results[0]['concept_uri']
-    conceptPropre["article_list"] = article_list_clean
-
-    return conceptPropre
-    
-# étape 4
-def write_clean_concept_to_JSON(cleanConcept, jsonName):
-    with open(jsonName+".json", "w") as cleanConceptJSONDoc:
-        json.dump(cleanConcept, cleanConceptJSONDoc)
+    # étape 4
+    def write_clean_concept_to_JSON(self, cleanConcept, jsonName):
+        with open(self.repo_path_clean+jsonName+".json", "w") as cleanConceptJSONDoc:
+            json.dump(cleanConcept, cleanConceptJSONDoc)
 
 
-# Fonction d'orchestration pour la requête type/concept
-def type_concept_to_clean_Json(concept, type):
-    """
-    Fonction complète qui récupère le concept de type descriptor/geo/org/per et crée et stock le JSON clean avec ce concept
-    """
-    #conceptString = getConceptDes(concept)
-    conceptString = get_concept(concept, type)
-    #print(conceptString)
+    # Fonction d'orchestration pour la requête type/concept
+    def type_concept_to_clean_Json(self, concept, type):
+        """
+        Fonction complète qui récupère le concept de type descriptor/geo/org/per et crée et stock le JSON clean avec ce concept
+        """
+        #conceptString = getConceptDes(concept)
+        conceptString = self.get_concept(concept, type)
+        #print(conceptString)
 
-    rawJSON = write_raw_concept_to_JSON(conceptString, concept)
-    #print(rawJSON)
+        rawJSON = self.write_raw_concept_to_JSON(conceptString, concept)
+        #print(rawJSON)
 
-    if (rawJSON['num_results'] != 0):
-        dictConceptPropre = get_elements_from_concept(rawJSON)
-        #pprint(dictConceptPropre)
-        write_clean_concept_to_JSON(dictConceptPropre, concept)
-    else:
-        print("Le concept: " + concept +  " n'a pas été trouvé dans l'API du NY Times")
+        if (rawJSON['num_results'] != 0):
+            dictConceptPropre = self.get_elements_from_concept(rawJSON)
+            #pprint(dictConceptPropre)
+            self.write_clean_concept_to_JSON(dictConceptPropre, concept)
+        else:
+            print("Le concept: " + concept +  " n'a pas été trouvé dans l'API du NY Times")
 
 
 
 # B: requête search
-# étape 1    
-def search_concept(concept):
-    """
-    On utilise l'api semantic avec la méthode GET /search.json
-    """
-    #print(concept)
-    
-    conceptName = concept
+    # étape 1    
+    def search_concept(self, concept):
+        """
+        On utilise l'api semantic avec la méthode GET /search.json
+        """
+        #print(concept)
+        
+        conceptName = concept
 
-    url_base = "http://api.nytimes.com/svc/semantic/v2/concept/search.json?query="
-    url_api_concept = url_base + conceptName
-    print(url_api_concept)
-    searchConcept = requests.get(url_api_concept, params=params_req)
-    return searchConcept
+        url_base = "http://api.nytimes.com/svc/semantic/v2/concept/search.json?query="
+        url_api_concept = url_base + conceptName
+        print(url_api_concept)
+        searchConcept = requests.get(url_api_concept, params=self.params_req)
+        return searchConcept
 
-# étape 2
-def write_raw_search_to_JSON(requestSearch, rawJsonName):
-    """
-    Stockage dans un fichier "ConceptRaw.json"
-    """
-    searchJSON = requestSearch.json()
-    with open("../data/raw_data/searchRaw_"+rawJsonName+".json", "w") as searchJSONDoc:
-        json.dump(searchJSON, searchJSONDoc)
-        return searchJSON
+    # étape 2
+    def write_raw_search_to_JSON(self, requestSearch, rawJsonName):
+        """
+        Stockage dans un fichier "ConceptRaw.json"
+        """
+        searchJSON = requestSearch.json()
+        with open(self.repo_path_raw+"searchRaw_"+rawJsonName+".json", "w") as searchJSONDoc:
+            json.dump(searchJSON, searchJSONDoc)
+            return searchJSON
 
-# étape 3
-def get_elements_from_search(rawJSONSearch, searchName):
-    """
-    On prend en paramètre la liste brut des résultats de la recherche et on renvoi un tableau de dictionnaire de concept.
-    Chacun de ces dictionnaires aura les éléments suivants:
-    -concept_id
-    -concept_name
-    -concept_created
-    -concept_type
-    """
-    
-    nombre_resultats = rawJSONSearch['num_results']
-    #print(nombre_resultats)
-    results = rawJSONSearch['results']
-    
-    clean_concepts = []
+    # étape 3
+    def get_elements_from_search(self, rawJSONSearch, searchName):
+        """
+        On prend en paramètre la liste brut des résultats de la recherche et on renvoi un tableau de dictionnaire de concept.
+        Chacun de ces dictionnaires aura les éléments suivants:
+        -concept_id
+        -concept_name
+        -concept_created
+        -concept_type
+        """
+        
+        nombre_resultats = rawJSONSearch['num_results']
+        #print(nombre_resultats)
+        results = rawJSONSearch['results']
+        
+        clean_concepts = []
 
-    for index, concept in zip(range(nombre_resultats), results):
-        c = {}
-        c["search_name"] = searchName
-        c['_id'] = concept['concept_id']
-        c['concept_name'] = concept['concept_name']
-        c['concept_created'] = concept['concept_created']
-        c['concept_type'] = concept['concept_type']
-        if(c['concept_type'] in ('nytd_des','nytd_org','nytd_per','nytd_geo')):
-            clean_concepts.append(c)
-            
-    return clean_concepts
-
-
-# étape 4
-def write_clean_search_to_JSON(cleanSearch, jsonName):
-    with open(jsonName+".json", "w") as cleanSearchJSONDoc:
-        json.dump(cleanSearch, cleanSearchJSONDoc)
+        for index, concept in zip(range(nombre_resultats), results):
+            c = {}
+            c["search_name"] = searchName
+            c['_id'] = concept['concept_id']
+            c['concept_name'] = concept['concept_name']
+            c['concept_created'] = concept['concept_created']
+            c['concept_type'] = concept['concept_type']
+            if(c['concept_type'] in ('nytd_des','nytd_org','nytd_per','nytd_geo')):
+                clean_concepts.append(c)
+                
+        return clean_concepts
 
 
-# Fonction d'orchestration pour la requête search
-def search_to_clean_Json(searchString):
-    """
-    Fonction complète qui récupère le string de recherche et crée et stock le JSON clean avec la liste des concepts contenants ce mot
-    """
-    searchConcept1 = search_concept(searchString)
-    rawJSONSearch1 = write_raw_search_to_JSON(searchConcept1, searchString)
-    cleanSearch1 = get_elements_from_search(rawJSONSearch1, searchString)
-    #print(cleanSearch1)
-    write_clean_search_to_JSON(cleanSearch1, searchString)
+    # étape 4
+    def write_clean_search_to_JSON(self, cleanSearch, jsonName):
+        with open(self.repo_path_clean+jsonName+".json", "w") as cleanSearchJSONDoc:
+            json.dump(cleanSearch, cleanSearchJSONDoc)
+
+
+    # Fonction d'orchestration pour la requête search
+    def search_to_clean_Json(self, searchString):
+        """
+        Fonction complète qui récupère le string de recherche et crée et stock le JSON clean avec la liste des concepts contenants ce mot
+        """
+        searchConcept1 = self.search_concept(searchString)
+        rawJSONSearch1 = self.write_raw_search_to_JSON(searchConcept1, searchString)
+        cleanSearch1 = self.get_elements_from_search(rawJSONSearch1, searchString)
+        #print(cleanSearch1)
+        self.write_clean_search_to_JSON(cleanSearch1, searchString)
 
 
 
 # test requête type/concept
+semantic = ApiSemantic("../data/raw_data/", "../data/clean_data/")
+
 typeDes = "nytd_des"
 typeGeo = "nytd_geo"
 typePer = "nytd_per"
 typeOrg = "nytd_org"
 
 conceptDes1 = "Coronavirus (2019-nCoV)"
-type_concept_to_clean_Json(conceptDes1, typeDes)
+#semantic.get_concept(conceptDes1,typeDes)
+semantic.type_concept_to_clean_Json(conceptDes1, typeDes)
 
 conceptDes2 = "Baseball"
-type_concept_to_clean_Json(conceptDes2, typeDes)
+semantic.type_concept_to_clean_Json(conceptDes2, typeDes)
 
 conceptGeo = "Acapulco (Mexico)"
-type_concept_to_clean_Json(conceptGeo, typeGeo)
+semantic.type_concept_to_clean_Json(conceptGeo, typeGeo)
 
 conceptPer = "Abbas, Mahmoud"
-type_concept_to_clean_Json(conceptPer, typePer)
+semantic.type_concept_to_clean_Json(conceptPer, typePer)
 
 conceptOrg = "Chicago White Sox"
-type_concept_to_clean_Json(conceptOrg, typeOrg)
+semantic.type_concept_to_clean_Json(conceptOrg, typeOrg)
 
 conceptInexistant = "Baskurt, Can"
-type_concept_to_clean_Json(conceptInexistant, typePer)
+semantic.type_concept_to_clean_Json(conceptInexistant, typeDes)
 
 
 # test requête search
 searchString = "Coronavirus"
-search_to_clean_Json(searchString)
+semantic.search_to_clean_Json(searchString)
