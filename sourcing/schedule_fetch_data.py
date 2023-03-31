b12d84a30778baddae5c60eab25a60cf722340a5
Mickael GASPAR <migatsar@gmail.com>
2023-03-31 15:43:27 +0200

    Refactor code syntax

--- sourcing/schedule_fetch_data.py
+++ sourcing/schedule_fetch_data.py
@@ -5,23 +5,17 @@ import schedule
 from api_most_popular import ApiMostPopular
 
 
-def fetch_data(endpoint="viewed", period=1):
-
-    endpoint = endpoint
-    period = period
-    api_most_popular = ApiMostPopular(repo_path="./data/raw_data/most_popular/")
-
-    response = api_most_popular.get_data(endpoint, period)
-
-    if response:
-        api_most_popular.save_data(response, endpoint, period)
-
-
 def job():
-
+    """Query NY Times API to fetch data of the previous day.
+    """
     endpoints = ["emailed", "shared", "viewed"]
+    api_popular = ApiMostPopular(repo_path="./data/raw_data/most_popular/")
+
     for e in endpoints:
-        fetch_data(e)
+        req = api_popular.get_data(endpoint=e, period=1)
+
+        if req:
+            api_popular.save_data(data=req, endpoint=e, period=1)
 
 
 # Run job every day at 8:00 until the 25/05/2023
@@ -31,7 +25,3 @@ schedule.every().day.at("08:00").until(datetime(2023, 5, 25)).do(job)
 while True:
     schedule.run_pending()
     time.sleep(5)
-
-
-# if __name__ == "__main__":
-#     main()
