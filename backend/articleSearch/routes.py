6ed94571eb2baa88e64d895eefec6eeec38abb0e
cafetierePhilo <guiraud.clement31@gmail.com>
2023-04-25 14:12:39 +0200

    endpoint article search api

---
+++ backend/articleSearch/routes.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter
+from database import mongodb
+from sourcing.request_article_search import ApiArticleSearch
+from typing import Optional
+
+
+# Instanciate a router for this endpoint
+routeAs = APIRouter()
+
+# Import ApiArticleSearch to request NYT API
+ArticleSearch = ApiArticleSearch()
+
+# Connection to mongodb collection
+articles = mongodb["Articles"]
+
+# Requêtes ArticleSearch
+@routeAs.get('/articleSearchApi', name="Requête ArticleSearch JSON", tags=['Requête cas métier n°1'])
+def requestArticleSearch(keyWord : str, fileName : str, filter : Optional[str]="" ):
+    ArticleSearch.request(keyWord, fileName, filter)
+    return "Fichier(s) bien récupéré(s)"
