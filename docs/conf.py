36a07f7d995b0e8c35c1829741cf8edda71441a2
Mickael GASPAR <migatsar@gmail.com>
2023-04-30 01:19:17 +0200

    Package ready

--- docs/conf.py
+++ docs/conf.py
@@ -6,10 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'DST-NewsFeed'
-copyright = '2023, Mickael GASPAR'
-author = 'Mickael GASPAR'
-release = '0.1.0'
+project = "DST-NewsFeed"
+copyright = "2023, Mickael GASPAR"
+author = "Mickael GASPAR"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -19,16 +19,15 @@ release = '0.1.0'
 # sys.path.insert(0, os.path.abspath('.'))
 
 extensions = [
-    'sphinx.ext.autodoc',
+    "sphinx.ext.autodoc",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
