367364477407b2fb4dfa0332271f5987380bc4e8
Mickael GASPAR <migatsar@gmail.com>
2023-04-30 12:57:15 +0200

    Recupere les infos populaires du jour via un job automatique

--- sourcing/schedule_fetch_data.py
+++ sourcing/schedule_fetch_data.py
@@ -1,17 +1,18 @@
-import time
 from datetime import datetime
+import time
 
 import schedule
-from api_most_popular import ApiMostPopular
+
+# for local imports
 import sys
-sys.path.insert(0,"../db")
+sys.path.insert(0, "../db")
 
 from bdd_write import Bdd
+from api_most_popular import ApiMostPopular
 
 
 def job():
-    """Query NY Times API to fetch data of the previous day.
-    """
+    """Query NY Times API to fetch data of the previous day."""
     endpoints = ["emailed", "shared", "viewed"]
     api_popular = ApiMostPopular(repo_path="../data/raw_data/most_popular/")
 
@@ -19,15 +20,17 @@ def job():
         req = api_popular.get_data(endpoint=e, period=1)
 
         if req:
-            raw_data_most_popular = api_popular.save_data(data=req, endpoint=e, period=1)
+            raw_data_most_popular = api_popular.save_data(
+                data=req, endpoint=e, period=1
+            )
             to_treat = raw_data_most_popular[0]
-            name = raw_data_most_popular[1] 
-        
+            name = raw_data_most_popular[1]
+
         to_stock = api_popular.clean_data(to_treat, name)
         most_popular_clean = api_popular.save_clean_data(to_stock, e, 1)[1]
 
         bddMostPopular = Bdd("../data/clean_data/most_popular/")
-        db = bddMostPopular.Client['NYTimes']
+        db = bddMostPopular.Client["NYTimes"]
         searchMostPopular = db["SearchMostPopular"]
         bddMostPopular.insert_mongoDB(most_popular_clean, searchMostPopular)
 
